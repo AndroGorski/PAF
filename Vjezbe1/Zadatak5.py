ad38bcdd52686cc6798847dcfad00f3e5d8c329b
AndroGorski <agorski1@pmfst.hr>
2026-03-17 12:56:08 +0100

    5.zadatak

---
+++ Vjezbe1/Zadatak5.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+def gibanje(F,m):
+    ak=F/m
+    t=[1,2,3,4,5,6,7,8,9,10]
+    a=[]
+    v=[]
+    x=[]
+    for i in range(1,11):
+        r=ak*i
+        v.append(r)
+        z=(ak*i**2)/2
+        x.append(z)
+        a.append(ak)
+    plt.subplot(3,2,1)
+    plt.title("x-t graf")
+    plt.xlabel("t/s")
+    plt.ylabel("x/m")
+    plt.plot(t,x)
+
+    plt.subplot(3,2,2)
+    plt.title("v-t graf")
+    plt.xlabel("t/s")
+    plt.ylabel("v/m*s**-1")
+    plt.plot(t,v)
+
+    plt.subplot(3,2,3)
+    plt.title("a-t graf")
+    plt.xlabel("t/s")
+    plt.ylabel("a/m*s**-2")
+    plt.plot(t,a)
+    plt.tight_layout()
+    plt.show()
+gibanje(10,5)
